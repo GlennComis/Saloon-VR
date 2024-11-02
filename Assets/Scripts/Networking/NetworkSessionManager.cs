e9452997d929b7b8410bee9b2d5dc1ba2d49a48f
Timon <t.nap@innovam.nl>
2024-11-02 17:19:40 +0100

    Further work on Network Setup

--- Assets/Scripts/Networking/NetworkSessionManager.cs
+++ Assets/Scripts/Networking/NetworkSessionManager.cs
@@ -5,13 +5,42 @@ using Unity.Netcode;
 
 public class NetworkSessionManager : SingletonMonoBehaviour<NetworkSessionManager>
 {
+    public enum BuildType
+    {
+        Server,
+        Host,
+        Client,
+    }
+    
     public NetworkManager networkManager;
     public Dictionary<ulong, PlayerController> players = new Dictionary<ulong, PlayerController>();
     public PlayerController localPlayer;
+    public BuildType buildType;
 
     protected override void Awake()
     {
         base.Awake();
         networkManager = GetComponent<NetworkManager>();
+    }
+    
+    private void Start()
+    {
+        StartInstance();
+    }
+
+    private void StartInstance()
+    {
+        switch (buildType)
+        {
+            case BuildType.Server:
+                networkManager.StartServer();
+                break;
+            case BuildType.Client:
+                networkManager.StartClient();
+                break;
+            case BuildType.Host:
+                networkManager.StartHost();
+                break;
+        }
     }
 }
