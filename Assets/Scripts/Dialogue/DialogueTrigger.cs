7d87fb59f499216a3ad930585a62f877796c1e2c
Glenn <glenncomis@outlook.com>
2024-11-02 22:10:51 +0100

    Added dialogue triggers and animated text
    Added dialogue triggers and animated text

---
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public bool dialogueEndTrigger;
+    
+    [SerializeField]
+    private ConversationScriptableObject conversationScriptableObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (dialogueEndTrigger)
+            {
+                DialogueManager.Instance.DoEndConversation();
+                return;
+            }
+            
+            DialogueManager.Instance.SetCurrentConversation(conversationScriptableObject);
+            DialogueManager.Instance.StartConversation();
+        }
+        else
+        {
+            Debug.Log(other.gameObject.name, other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DialogueManager.Instance.DoEndConversation();
+        }
+    }
+}
