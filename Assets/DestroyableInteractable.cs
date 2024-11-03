75aea89b4c15f5c207af479f92cde430740624ad
Glenn <glenncomis@outlook.com>
2024-11-03 14:38:46 +0100

    Added frame, bottle and being able to shoot and destroy items
    - Added frame, bottle and being able to shoot and destroy items

---
+++ Assets/DestroyableInteractable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyableInteractable : SoundInteractable
+{
+    private MeshRenderer meshRenderer;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public override void OnShotHit()
+    {
+        base.OnShotHit();
+
+        if (meshRenderer != null)
+        {
+            meshRenderer.enabled = false; // Disable the mesh renderer
+        }
+
+        StartCoroutine(DestroyAfterSound());
+    }
+
+    private IEnumerator DestroyAfterSound()
+    {
+        if (audioSource != null && shotSound != null)
+        {
+            while (audioSource.isPlaying)
+            {
+                yield return null;
+            }
+        }
+        Destroy(gameObject);
+    }
+}
